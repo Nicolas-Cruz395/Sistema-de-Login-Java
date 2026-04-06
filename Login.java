1370ce81331844a6c75097f83b23d2a7b5192f14
Nicolas Renato <nrenato061@gmail.com>
2026-04-06 15:06:08 -0300

    Login.java

--- Login.java
+++ Login.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Login{
+public class Logando{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
 
@@ -8,62 +8,69 @@ public class Login{
         String user_criado = "";
         String senha_criada = "";
         int idade = 0;
-        boolean sistema = true;
-        int tentativa = 0;
         int limite = 3;
+        int tentativa = 0;
+        String user_digitado = "";
+        String senha_digitada = "";
+
 
         System.out.println("Qual seu nome? ");
         nome = sc.nextLine();
 
-        System.out.print("Qual sua idade? ");
+        System.out.println("Digite sua idade: ");
         idade = sc.nextInt();
         sc.nextLine();
 
         if(idade < 18){
-        System.out.println("Acesso não Liberado, Você é menor de idade.");
-        return;
+            System.out.println("Você é menor de idade, acesso não autorizado.");
+            return;
         } else {
-        System.out.println("");
+            System.out.printf("Olá %s, Seja muito bem vindo!", nome);
         }
 
-        System.out.printf("Olá %s Seja Bem Vindo!\n", nome);
-
-        System.out.println("Crie um User: ");
+        System.out.println("\nCrie um User: ");
         user_criado = sc.nextLine();
 
-        System.out.println("Crie uma Senha: ");
+        System.out.println("Crie uma senha: ");
         senha_criada = sc.nextLine();
 
-        while (sistema){
+        System.out.println("\nUsuario e senha de acesso criado com sucesso!");
+
+        while (tentativa < limite){
 
             System.out.println("Digite seu User: ");
-            String user = sc.nextLine();
-            
-            System.out.println("Digite sua Senha: ");
-            String senha = sc.nextLine();
+            user_digitado = sc.nextLine();
 
-            if(user.equals(user_criado) && senha.equals(senha_criada)){
-                System.out.println("Acesso Autorizado!");
-                sistema = false;
+            System.out.println("Digite sua senha: ");
+            senha_digitada = sc.nextLine();
 
-            } else if(!user.equals(user_criado) && senha.equals(senha_criada)){
-                System.out.println("User Incorreto!");
-                tentativa++;
+            if(user_digitado.equals(user_criado) && senha_digitada.equals(senha_criada)){
+              System.out.printf("Acesso Autorizado!\nSeja bem vindo ao nosso servidor %s", nome);
+              break;
 
-            } else if(user.equals(user_criado) && !senha.equals(senha_criada)){
-                System.out.println("Senha Incorreta!");
-                tentativa++;
+        }   else if(!user_digitado.equals(user_criado) && senha_digitada.equals(senha_criada)){
+              System.out.println("User incorreto!");
+              tentativa++;
 
-            } else {
-                System.out.println("User e Senha Incorretos!");
-                tentativa++;
-            }
-            if (tentativa >= limite){
-                System.out.println("Limite de tentativa ultrapassado!");
-                sistema = false;
-            } else {
-                System.out.printf("Você tem %d tentativas restante.\n", (limite - tentativa));
-            }
-        }   
+        }   else if(user_digitado.equals(user_criado) && !senha_digitada.equals(senha_criada)){
+              System.out.println("Senha incorreta!");
+              tentativa++;
+
+        }   else {
+              System.out.println("Usuario e Senha Incorreto!");
+              tentativa++;
+        }
+
+            if(tentativa >= limite){
+               System.out.println("Limite de tentativas ultrapassado, Acesso bloqueado!");
+               break;
+
+        }   else {
+              System.out.printf("Você tem %d tentativa(s) restante(s).\n", (limite - tentativa));
+        }
+        }
+
+        sc.close();
+
     }
 }
